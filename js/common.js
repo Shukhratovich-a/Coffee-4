e584b5f548f4c10868dd815f1ac08e9c4fad0085
Shukhratovich-a <shukhratovich138@gmail.com>
2021-12-03 18:45:41 +0500

    Edit header section

--- js/common.js
+++ js/common.js
@@ -4,9 +4,21 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	function toggleMenu() {
 		$('.nav__burger').toggleClass('nav__burger--active')
-		$('.nav__list').toggleClass('.nav__list--active')
+		$('.nav__list').toggleClass('nav__list--active')
+        $('.body').toggleClass('body--active')
 	}
 
 	$('.nav__burger').click(function() { toggleMenu() })
 
+
+	function closeMenu() {
+		$('.nav__burger').removeClass('nav__burger--active')
+		$('.nav__list').removeClass('nav__list--active')
+		$('.body').removeClass('body--active')
+	}
+
+	$(document).click(function(e) {
+		if ($(e.target).closest('.nav').length) return
+		closeMenu()
+	})
 })
