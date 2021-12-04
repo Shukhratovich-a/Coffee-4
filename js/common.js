c5b95c8c128602cc69b3907bc7aa2f59b9ffba2a
Shukhratovich-a <shukhratovich138@gmail.com>
2021-12-05 00:46:44 +0500

    Edit nav bar

--- js/common.js
+++ js/common.js
@@ -1,24 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-
-    // Menu
-
+    
     function toggleMenu() {
-        $('.header .nav__burger').toggleClass('nav__burger--active')
-        $('.header .nav__list').toggleClass('nav__list--active')
-        $('.body').toggleClass('body--active')
+        $('.header').toggleClass('header--active')
     }
 
     $('.nav__burger').click(function () { toggleMenu() })
-
-
-    function closeMenu() {
-        $('.nav__burger').removeClass('nav__burger--active')
-        $('.nav__list').removeClass('nav__list--active')
-        $('.body').removeClass('body--active')
-    }
-
-    $(document).click(function (e) {
-        if ($(e.target).closest('.nav').length) return
-        closeMenu()
-    })
 })
